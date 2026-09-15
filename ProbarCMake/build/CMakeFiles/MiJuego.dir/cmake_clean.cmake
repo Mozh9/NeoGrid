b0512d247f17ee27e9e5efb8aecace4b6bb2843c
Mozh9 <jaz93jmonter@gmail.com>
2026-09-15 00:38:39 -0600

    Actualización de proyecto, Prueba con CMake

---
+++ ProbarCMake/build/CMakeFiles/MiJuego.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiJuego.dir/main.cpp.obj"
+  "CMakeFiles/MiJuego.dir/main.cpp.obj.d"
+  "MiJuego.exe"
+  "MiJuego.exe.manifest"
+  "MiJuego.pdb"
+  "libMiJuego.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiJuego.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ProbarCMake/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main (){
+
+	std::cout <<"Hellow World Pureba CMake Para juego RPG_Consola. Todo en orden" << std::endl;
+	std::cout <<"Presiona Enter para salir..." << std::endl;
+	std::cin.get();
+	return 0;
+}
