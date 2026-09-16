79282231f588a6adc51d86e41a0b0131df3a5154
Mozh9 <jaz93jmonter@gmail.com>
2026-09-16 08:37:59 -0600

    Primer commit de esta branch y creación de neoMain.cpp

---
+++ src/neoMain.cpp
@@ -0,0 +1,9 @@
+/*Estoy dividiendo el main actual en más archivos para tener un proyecto modular donde
+ * más personas puedan trabajar sin que nada explote, este sería el nuevo main
+ * También parte de esta depuración de código es comentar y documentar todo 
+ * (de documentar la verdad no c porque qué hueva, pero sí deberíamos)
+ * Lo que sí es que plis comenten TODO, TODO lo que se les venga a la cabeza*/
+
+int main (){
+	return 0;
+}
