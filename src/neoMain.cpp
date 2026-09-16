af9e7af611a800379f385bd565404b567ce96cca
Mozh9 <jaz93jmonter@gmail.com>
2026-09-16 09:07:50 -0600

    Escribir idea general en neoMain y editar CMakeLists para ejecutar neoMain.cpp y no main.cpp

--- src/neoMain.cpp
+++ src/neoMain.cpp
@@ -1,9 +1,30 @@
 /*Estoy dividiendo el main actual en más archivos para tener un proyecto modular donde
- * más personas puedan trabajar sin que nada explote, este sería el nuevo main
+ * más personas puedan trabajar sin que nada explote, este sería el nuevo main.
  * También parte de esta depuración de código es comentar y documentar todo 
  * (de documentar la verdad no c porque qué hueva, pero sí deberíamos)
- * Lo que sí es que plis comenten TODO, TODO lo que se les venga a la cabeza*/
+ * Lo que sí es que plis comenten TODO, TODO lo que se les venga a la cabeza
+ * Tal vez después platicamos de Cómo hacer los comentarios para que cualqueira que quiera
+ * hecharle mano pueda hacerlo, pero por ahora solo haganlo, plis.
+ * Una última cosa, de ahora en adelante tristemente habrá que comentar en ingles;
+ * Más que nada porque sí quiero hacer este proyecto bien y si en un futuro alguien
+ * en git o lo que sea nos quiere ayudar, entonces si se ocupa que esté en ingles. T-T
+ * Me siento como si vendiera mi alama al diablo
+ * Bueno, pues eso, x; cualqueir cosa diganme :).*/
+
+//#include"include/game_engine.hpp"
+#include <iostream> //elimnate later, testing cMake
 
 int main (){
+	//Only the universal stuff a program does: ask for input, process it and print a result
+	//General idea of how it will work
+
+	//GameEngine engine;
+	//engine.init();
+	//engine.load();
+	//engine.run();
+	//engine.end();
+	
+	std::cout << "El programa funciona, cMake no hizo nada raro" << std::endl;
+
 	return 0;
 }
